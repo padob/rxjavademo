43df3223bc8b2d876b1901fa43119e59bf84aea2
Pawel Dobrzanski <pawel.dobrzanski.ctr@sabre.com>
2019-01-26 18:31:38 +0100

    basic factories

--- src/test/java/ObserversTest.java
+++ src/test/java/ObserversTest.java
@@ -1,6 +1,7 @@
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.observables.ConnectableObservable;
 import org.junit.Test;
 
 import static java.util.Arrays.asList;
@@ -88,5 +89,62 @@ public class ObserversTest {
 
         Observable<String> sourceSecond = Observable.fromIterable(asList("11", "22", "33"));
         sourceSecond.subscribe(e -> System.out.println("Observer 1 " + e));
+    }
+
+    @Test
+    public void basicObserver_2_9() {
+        Observable<String> sourceSecond = Observable.fromIterable(asList("11", "22", "33"));
+        ConnectableObservable<String> hot = sourceSecond.publish();
+
+        Disposable subscribe = hot.subscribe(e -> System.out.println("Observer 1 " + e));
+        Disposable subscribe2 = hot.subscribe(e -> System.out.println("Observer 2 " + e));
+
+
+        hot.connect();
+    }
+
+    @Test
+    public void basicObserver_2_10() {
+//        Observable.interval(100, TimeUnit.MILLISECONDS).subscribe(System.out::println);
+//        try {
+//            Thread.sleep(5000);
+//        }
+//        catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+        Observable.range(1, 10).subscribe(System.out::println);
+        try {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //for debugging!
+    @Test
+    public void basicObserver_error_factory() {
+        Observable.error(new Exception("Crash!"))
+                  .subscribe(System.out::println,
+                             Throwable::printStackTrace,
+                             () -> {
+                                 System.out.println("done");
+                             });
+    }
+
+    @Test
+    public void basicObserver_empty_never_factory() {
+        Observable.empty();
+        Observable.never();
+
+//        Future<String> futureValue =...;
+//        Observable.fromFuture(futureValue)
+//                  .map(String::length)
+//                  .subscribe(System.out::println);
+    }
+
+    @Test
+    public void basicObserver_XXX_XXX() {
     }
 }
